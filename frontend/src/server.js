8c7d976764c63137749617fb1b496d0ed5bbd67c
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-09-28 17:01:04 +0300

    updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:9000/api/v2";
+export const server = "https://mern-web-yn5l.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:9000/";
+export const backend_url = "https://mern-web-yn5l.onrender.com/";
