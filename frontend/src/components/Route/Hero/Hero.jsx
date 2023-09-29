17c49a358b7f773606c2a799c9a96693a364a087
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-09-29 09:45:02 +0300

    updated

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../../styles/styles";
-
+import {slideData} from "../../../static/data"
 const Hero = () => {
   return (
     <div
