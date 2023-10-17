e38473a660586401c335766cae365a31e1cff66d
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-10-17 23:46:05 +0300

    updated

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <>
     <div
-      className={`  h-[25vh] 800px:h-[35vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      className={`relative  min-h-[50vh] 800px:min-h-[80vh] w-[50%] bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
           "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
