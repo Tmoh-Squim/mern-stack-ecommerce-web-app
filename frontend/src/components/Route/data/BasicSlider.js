dc89247023c66f06457e88afc60f4318fefd7d12
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-10-17 21:22:40 +0300

    updated

--- frontend/src/components/Route/data/BasicSlider.js
+++ frontend/src/components/Route/data/BasicSlider.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import HeroSlider,{Slide} from 'hero-slider'
 
+//images
+const img1="https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg"
+const img2="https://cdn.openpr.com/T/c/Tc15444071_g.jpg"
+const img3="https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png"
+const img4="https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png"
 const BasicSlider = () => {
-    const img1="https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg"
-    const img2="https://cdn.openpr.com/T/c/Tc15444071_g.jpg"
-    const img3="https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png"
-    const img4="https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png"
   return (
     <>
     <HeroSlider
