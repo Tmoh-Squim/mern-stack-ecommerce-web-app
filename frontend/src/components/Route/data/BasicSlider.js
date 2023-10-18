cddc725445a059031e3348de6aaa164a309d8cd1
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-10-18 09:17:07 +0300

    updated

--- frontend/src/components/Route/data/BasicSlider.js
+++ frontend/src/components/Route/data/BasicSlider.js
@@ -35,28 +35,28 @@ const BasicSlider = () => {
             backgroundImageSrc:img1,
             backgroundAttachment:"fixed"
         }}
-        style={{height:"35vh",margin:"0.8rem"}}
+        className="h-[15vh] 800px:h-[35vh]"
         />
         <Slide 
         background={{
             backgroundImageSrc:img2,
             backgroundAttachment:"fixed"
         }}
-        style={{height:"35vh",margin:"0.8rem"}}
+        className="h-[15vh] 800px:h-[35vh]"
         />
         <Slide 
         background={{
             backgroundImageSrc:img3,
             backgroundAttachment:"fixed"
         }}
-        style={{height:"35vh",margin:"0.8rem"}}
+        className="h-[15vh] 800px:h-[35vh]"
         />
         <Slide 
         background={{
             backgroundImageSrc:img4,
             backgroundAttachment:"fixed"
         }}
-        style={{height:"35vh",margin:"0.8rem"}}
+        className="h-[15vh] 800px:h-[35vh]"
         />
         <Nav />
     </HeroSlider>
