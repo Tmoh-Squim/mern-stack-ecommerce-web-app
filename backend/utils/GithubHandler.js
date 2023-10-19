2d461d84079f6f7a8c80e0a2bc148069d35e876c
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-10-19 11:02:15 +0300

    updated

--- backend/utils/GithubHandler.js
+++ backend/utils/GithubHandler.js
@@ -1,7 +1,7 @@
 const { Octokit } = require('@octokit/rest');
-const fs = require("fs")
+const fs = require('fs').promises;
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN, // Use your GitHub personal access token
+  auth: process.env.GITHUB_TOKEN,
 });
 
 async function commitToGitHub(fileUrl) {
@@ -15,65 +15,75 @@ async function commitToGitHub(fileUrl) {
 
     const defaultBranch = repo?.data?.default_branch || 'main';
 
-    // Fetch the latest changes from GitHub
     const latestCommitOnRemote = (await octokit.git.getRef({
       owner: 'Tmoh-Squim',
       repo: 'mern-stack-ecommerce-web-app',
       ref: `heads/${defaultBranch}`,
     })).data.object.sha;
 
-    const content = await fs.readFile(fileUrl);
-    const tree = await octokit.git.createTree({
-      owner: 'Tmoh-Squim',
-      repo: 'mern-stack-ecommerce-web-app',
-      base_tree: latestCommitOnRemote, // Use the latest commit on the default branch
-      tree: [
-        {
-          path: `backend/uploads/${fileUrl}`,
-          mode: '100644',
-          type: 'blob',
-          content,
-        },
-      ],
-    });
-
-    const parents = [latestCommitOnRemote]; // Set the parent commit to the latest commit on the default branch
-
-    const commit = await octokit.git.createCommit({
-      owner: 'Tmoh-Squim',
-      repo: 'mern-stack-ecommerce-web-app',
-      message: 'Add uploaded file',
-      tree: tree.data.sha,
-      parents,
-    });
-
-    // Attempt to update the main branch (or the default branch) with the new commit
-    try {
-      await octokit.git.updateRef({
+    // Check if the file exists before reading it
+    if (await fileExists(fileUrl)) {
+      const content = await fs.readFile(fileUrl);
+      const tree = await octokit.git.createTree({
         owner: 'Tmoh-Squim',
         repo: 'mern-stack-ecommerce-web-app',
-        ref: `heads/${defaultBranch}`,
-        sha: commit.data.sha,
+        base_tree: latestCommitOnRemote,
+        tree: [
+          {
+            path: `backend/uploads/${fileUrl}`,
+            mode: '100644',
+            type: 'blob',
+            content,
+          },
+        ],
       });
-    } catch (error) {
-      console.error('Error updating the branch:', error);
 
-      // Handle the case where the update is not a fast forward (e.g., conflict with remote changes)
-      console.log('Merging changes from the remote branch into the local branch...');
-      // You can use octokit.repos.merge() to perform the merge
-      await octokit.repos.merge()
-      // After merging, try to push again
-      await octokit.git.updateRef({
+      const parents = [latestCommitOnRemote];
+
+      const commit = await octokit.git.createCommit({
         owner: 'Tmoh-Squim',
         repo: 'mern-stack-ecommerce-web-app',
-        ref: `heads/${defaultBranch}`,
-        sha: commit.data.sha,
+        message: 'Add uploaded file',
+        tree: tree.data.sha,
+        parents,
       });
+
+      try {
+        // Specify merge details, such as the base and head branches
+        await octokit.repos.merge({
+          owner: 'Tmoh-Squim',
+          repo: 'mern-stack-ecommerce-web-app',
+          base: defaultBranch,
+          head: 'main', // Replace with your branch name
+          commit_message: 'Merge feature branch into main', // Replace with your commit message
+        });
+
+        await octokit.git.updateRef({
+          owner: 'Tmoh-Squim',
+          repo: 'mern-stack-ecommerce-web-app',
+          ref: `heads/${defaultBranch}`,
+          sha: commit.data.sha,
+        });
+
+        console.log('File pushed to GitHub successfully.');
+      } catch (error) {
+        console.error('Error updating the branch:', error);
+      }
+    } else {
+      console.error('File does not exist:', fileUrl);
     }
-
-    console.log('File pushed to GitHub successfully.');
   } catch (error) {
     console.error('Error pushing file to GitHub:', error);
+  }
+}
+
+// Function to check if a file exists
+async function fileExists(filePath) {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
   }
 }
 
