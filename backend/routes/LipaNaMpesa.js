933cda290d3aba3bc907e73554130915d45f6440
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-10-24 22:39:18 +0300

    updated

--- backend/routes/LipaNaMpesa.js
+++ backend/routes/LipaNaMpesa.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const router = express.Router()
-import {
+const {
     initiateSTKPush,
     stkPushCallback,
     confirmPayment
 
 
-} from "../controller/lipaNaMpesa.js";
+} = require("../controller/lipaNaMpesa.js")
 
 
-import {accessToken} from "../middleware/genenerateAccessToken.js";
+const {accessToken} = require("../middleware/genenerateAccessToken.js")
 
 router.route('/stkPush').post(accessToken,initiateSTKPush)
 router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
