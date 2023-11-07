5f34de7d2067f367c5dd2d9e5c6faa233bc413ec
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-07 13:26:35 +0300

    updated

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -63,13 +63,13 @@ const ProductCard = ({ data,isEvent }) => {
 
   return (
     <>
-      <div className="w-[48%] h-[auto] mb-[2px] bg-white rounded-lg shadow-sm !pb-2 p-0  relative cursor-pointer sm:w-full">
+      <div className="w-[48%] h-[auto] mb-[2px] bg-white rounded-lg shadow-sm !pb-2 p-0 800px:p-3 relative cursor-pointer sm:w-full">
         <div className="flex justify-end"></div>
         <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
           <img
             src={`${data.images && data.images[0]}`}
             alt=""
-            className="w-full h-[170px] 800px:rounded-t-lg object-cover 800px:object-contain"
+            className="w-full h-[170px] rounded-t-lg 800px:rounded-t-none object-cover 800px:object-contain"
           />
         </Link>
         <div className="px-3">
