c2f1946147f1c57006f3f71c8aaa493cae2d15ba
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-07 16:02:59 +0300

    updated

--- frontend/src/components/Products/SuggestedProduct.jsx
+++ frontend/src/components/Products/SuggestedProduct.jsx
@@ -17,13 +17,13 @@ const SuggestedProduct = ({ data }) => {
   return (
     <div className="px-3">
       {data ? (
-        <div className={`p-4 ${styles.section}`}>
+        <div className={`p-2 ${styles.section}`}>
           <h2
             className={`${styles.heading} text-[25px] font-[500] border-b mb-5`}
           >
             Related Product
           </h2>
-          <div className="flex !overflow-x-scroll grid-cols-1 gap-[12px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+          <div className="flex mx-3 overflow-x-scroll scroll-smooth grid-cols-1 gap-[12px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
              {
                 productData && productData.map((i,index) => (
                     <ProductCard data={i} key={index} />
