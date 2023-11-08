4941897635be597df4dfb39ab81e5060edce4535
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-08 15:10:24 +0300

    updated

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -73,7 +73,7 @@ const Faq = () => {
               <p className="text-base text-gray-500">
                 If you're not satisfied with your purchase, we accept returns
                 within 30 days of delivery. To initiate a return, please email
-                us at support@myecommercestore.com with your order number and a
+                us at squimstech@gmail.com with your order number and a
                 brief explanation of why you're returning the item.
               </p>
             </div>
@@ -171,8 +171,8 @@ const Faq = () => {
             <div className="mt-4">
               <p className="text-base text-gray-500">
                 You can contact our customer support team by emailing us at
-                support@myecommercestore.com, or by calling us at (555) 123-4567
-                between the hours of 9am and 5pm EST, Monday through Friday.
+                squimstech@gmail.com, or by calling us at +254748143442
+                between the hours of 8am and 5pm EST, Monday to Friday.
               </p>
             </div>
           )}
@@ -221,7 +221,7 @@ const Faq = () => {
               <p className="text-base text-gray-500">
                 Unfortunately, once an order has been placed, we are not able to
                 make changes or cancellations. If you no longer want the items
-                you've ordered, you can return them for a refund within 30 days
+                you've ordered, you can return them for a refund within 1 week
                 of delivery.
               </p>
             </div>
@@ -269,7 +269,7 @@ const Faq = () => {
           {activeTab === 6 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                Currently, we only offer shipping within the United States.
+                Currently, we only offer shipping within Kenya.
               </p>
             </div>
           )}
@@ -316,7 +316,7 @@ const Faq = () => {
           {activeTab === 7 && (
             <div className="mt-4">
               <p className="text-base text-gray-500">
-                We accept visa,mastercard,paypal payment method also we have
+                We accept visa,mastercard,paypal,lipa na mpesa payment method also we have
                 cash on delivery system.
               </p>
             </div>
