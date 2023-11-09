7a0a112046f964c57bf87b6679d3838d9c82522f
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-09 16:31:11 +0300

    updated

--- frontend/src/components/Route/data/BasicSlider.js
+++ frontend/src/components/Route/data/BasicSlider.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeroSlider, { Slide, Nav } from "hero-slider";
 import "../../../App.css"
 //images
-const img1 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fecommerce-banner&psig=AOvVaw1s7nQbcnTEBHnXTOUShC5h&ust=1699622247413000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCP6siAt4IDFQAAAAAdAAAAABAE";
+const img1 = "https://images.app.goo.gl/tXa6b5N1iNTV4ST47";
 const img2 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.postermywall.com%2Findex.php%2Fart%2Ftemplate%2F67720435bb809be27f46dfb1dd44c6fa%2Fgrocery-sale-retail-or-e-commerce-banner-ad-design-template&psig=AOvVaw1s7nQbcnTEBHnXTOUShC5h&ust=1699622247413000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCP6siAt4IDFQAAAAAdAAAAABAI";
 const img3 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpsd%2Fe-commerce-banner&psig=AOvVaw1s7nQbcnTEBHnXTOUShC5h&ust=1699622247413000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCP6siAt4IDFQAAAAAdAAAAABAQ";
 const img4 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpsd%2Fe-commerce-banner&psig=AOvVaw2Njng2mh8JKZDUM826Evw9&ust=1699622452264000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODY9JyBt4IDFQAAAAAdAAAAABAE";
