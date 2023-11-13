3cd905dadb6926e7bb8231747e1212b7c7462a26
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-13 12:15:39 +0300

    updated

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -39,7 +39,7 @@ const Header = ({ activeHeading }) => {
     if(term !== ''){
       setSearchTerm(term);
     }else{
-      setSearchTerm('')
+      setSearchTerm(null)
     }
     
     const filteredProducts =
