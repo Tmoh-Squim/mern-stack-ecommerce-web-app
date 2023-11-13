f873538984cb4916108d3c5a2801cd1bbe389514
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-13 13:08:32 +0300

    updated

--- frontend/src/components/Wishlist/Wishlist.jsx
+++ frontend/src/components/Wishlist/Wishlist.jsx
@@ -25,8 +25,8 @@ const Wishlist = ({ setOpenWishlist }) => {
   }
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-30">
-      <div className="fixed top-0 right-0 h-full w-[100%] overflow-y-scroll 800px:w-[25%] bg-white flex flex-col justify-between shadow-sm">
+    <div className="fixed top-0 mb-[50px] left-0 w-full bg-[#0000004b] h-screen z-30">
+      <div className="fixed top-0 right-0  w-[100%] overflow-y-scroll 800px:w-[25%] bg-white flex flex-col justify-between shadow-sm">
         {wishlist && wishlist.length === 0 ? (
           <div className="w-full h-screen flex items-center justify-center">
             <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
