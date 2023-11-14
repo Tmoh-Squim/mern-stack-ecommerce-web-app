392a58dab28ffcaa36f2a102192b09fb3621e28e
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-14 13:37:15 +0300

    updated

--- frontend/src/components/Route/Hero/Carousel.jsx
+++ frontend/src/components/Route/Hero/Carousel.jsx
@@ -3,24 +3,14 @@ import { Carousel } from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import '../../../App.css'
 
-const slideData=[
-  {
-    id:'1',
-    image:'https://img.freepik.com/free-psd/horizontal-banner-online-fashion-sale_23-2148585404.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699401600&semt=ais'
-  },
-  {
-    id:'2',
-    image:'https://img.freepik.com/free-psd/special-sales-banner-template_23-2148975924.jpg'
-  },
-  {
-    id:'3',
-    image:'https://cdn5.vectorstock.com/i/1000x1000/52/04/online-shopping-e-commerce-banner-concept-vector-25035204.jpg'
-  },
-  {
-    id:'4',
-    image:'https://img.freepik.com/free-psd/online-shopping-concept-banner-template_23-2148559464.jpg'
-  }
-]
+const img1 =
+  "https://img.freepik.com/free-psd/horizontal-banner-online-fashion-sale_23-2148585404.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699401600&semt=ais";
+const img2 =
+  "https://img.freepik.com/free-psd/special-sales-banner-template_23-2148975924.jpg";
+const img3 =
+  "https://cdn5.vectorstock.com/i/1000x1000/52/04/online-shopping-e-commerce-banner-concept-vector-25035204.jpg";
+const img4 =
+  "https://img.freepik.com/free-psd/online-shopping-concept-banner-template_23-2148559464.jpg";
 class Carousele extends Component {
     constructor(props){
         super(props);
@@ -42,11 +32,7 @@ class Carousele extends Component {
     }
     render(){
   return (
-    <>
-    {
-      slideData && slideData.map((i)=>{
-        return (
-       <Carousel
+      <Carousel
         showArrows={true}
         onChange={this.onChange}
         onClickItem={this.onClickItem}
@@ -56,14 +42,19 @@ class Carousele extends Component {
         interval={3000}
         className="h-[135px] w-[94%] rounded-[9px] m-4 800px:h-[400px] 800px:w-[65%]"
       >
-      <div key={i.id}>
-            <img src={i.image} className="h-[135px] w-[100%] rounded-[9px]  800px:h-[390px] 800px:w-[50%]" alt="" />
-            </div>
+        <div>
+          <img src={img1} className="h-[135px] w-[100%] rounded-[9px]  800px:h-[390px] 800px:w-[50%]" alt="" />
+        </div>
+        <div>
+          <img src={img2} className="h-[135px] w-[100%] rounded-[9px]  800px:h-[390px] 800px:w-[50%]" alt="" />
+        </div>
+        <div>
+          <img src={img3} className="h-[135px] w-[100%] rounded-[9px]  800px:h-[390px] 800px:w-[50%]" alt="" />
+        </div>
+        <div>
+          <img src={img4} className="h-[135px] w-[100%] rounded-[9px]  800px:h-[390px] 800px:w-[50%]" alt="" />
+        </div>
       </Carousel>
-        )
-      })
-    }
-    </>
   );
 };
 }
