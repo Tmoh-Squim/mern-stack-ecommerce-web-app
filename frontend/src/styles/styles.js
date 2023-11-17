e00c9606873d5a1a8ae7e2ce4275e561971cdf7a
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-17 22:50:03 +0300

    updated

--- frontend/src/styles/styles.js
+++ frontend/src/styles/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     custom_container: "w-11/12 hidden sm:block",
     heading: 'text-[27px] text-center md:text-start font-[600] font-Roboto pb-[20px]',
-    section:'w-11/12 mx-auto',
+    section:'800px:w-11/12 w-[95%] mx-auto',
     productTitle: 'text-[25px] font-[600] font-Roboto text-[#333]',
     productDiscountPrice:"font-bold text-[15px] text-[#d55b45]  font-Roboto",
     price:"font-[500] text-[10px] ] pl-3 text-[#333] line-through",
