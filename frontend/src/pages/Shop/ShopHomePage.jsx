ce9eaea16a82ca68689ea5a3860613d77ffe23c0
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-17 21:50:25 +0300

    updated

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -6,8 +6,8 @@ import ShopProfileData from "../../components/Shop/ShopProfileData";
 const ShopHomePage = () => {
   return (
     <div className={`${styles.section} bg-[#f5f5f5]`}>
-         <div className="w-full flex py-10 justify-between">
-          <div className="w-full hidden 800px:block 800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
+         <div className="w-full flex flex-col py-10 justify-between">
+          <div className="w-full  800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
             <ShopInfo isOwner={true} />
           </div>
           <div className="w-full 800px:w-[72%] rounded-[4px]">
